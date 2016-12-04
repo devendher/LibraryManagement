0bd6b2b0fcbc44e1a9e5b7b4b063048916802af4
Devendher <macha.devendher35@gmail.com>
2016-12-04 19:31:37 +0530

    Added default due date

--- models/transactionalmodel.js
+++ models/transactionalmodel.js
@@ -3,13 +3,15 @@ var Schema=mongoose.Schema;
 
 var booksmodel=require('../models/booksmodel.js');
 
+var dt=new Date();
+var newdate=dt.setDate(dt.getDate() + 5);
 var transactions= new Schema({
 	bookid:[{ type: Schema.Types.ObjectId, ref: 'books' }],
 	username:{type:String},
 	email:{type:String},
 	contact:{type:Number},
 	IssueDate:{type:Date,default:Date.now},
-	DueDate:{type:Date},
+	DueDate:{type:Date,default:newdate},
 	status:{type:String,default:'Barrowed'}
 	
 });
